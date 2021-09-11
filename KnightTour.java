af3867cec5cb5e0cf54706809733b7cf3f89ec7f
hungtrieu07 <hungtrieu0701@gmail.com>
2021-09-11 08:04:18 +0700

    final

--- KnightTour.java
+++ KnightTour.java
@@ -1,75 +1,79 @@
-public class KnightTour {
-	private static int N = 8;
-	// thiết lập các ô vuông hợp lý để con mã có thể đặt trên bàn cờ
-	private static boolean isValid (int x, int y, int[][] board) {
-		return (x >= 0 && x < N && y >= 0 && y < N && board[x][y] == -1);
+class KnightTour {
+	private int count;
+    private int BOARD_SIZE;
+	private int[][] visited;
+	private int[] xMoves = { 2, 1, -1, -2, -2, -1, 1, 2 };
+	private int[] yMoves = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+	public KnightTour(int chessBoardSize) {
+		this.BOARD_SIZE = chessBoardSize;
+		this.visited = new int[BOARD_SIZE][BOARD_SIZE];
+		this.initializeBoard();
+	}
+	
+	private void initializeBoard() {
+		for (int i = 0; i < BOARD_SIZE; i++)
+			for (int j = 0; j < BOARD_SIZE; j++)
+				this.visited[i][j] = Integer.MIN_VALUE;
 	}
 
-	private static void printBoard(int[][] board) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.format("%3d ", board[i][j]);
+	public void solveKnightTourProblem() {
+		visited[0][0] = 0;
+		// bắt đầu từ ô cờ vị trí (0,0)
+		if( solveProblem(1, 0, 0) == false) {
+			System.out.println("Không có cách giải nào được tìm thấy...");
+		}
+	}
+
+	public boolean solveProblem(int moveCount, int x, int y) {
+		if (moveCount == BOARD_SIZE * BOARD_SIZE) {
+			printSolution();
+			return true;
+		}
+		
+		boolean res = false;
+		for (int i = 0; i < xMoves.length; ++i) {
+
+			int nextX = x + xMoves[i];
+			int nextY = y + yMoves[i];
+
+			// kiểm tra nếu vị trí mới hợp lệ và chưa được đi qua
+			if ( isValidMove(nextX, nextY) && visited[nextX][nextY] == Integer.MIN_VALUE) {
+
+				visited[nextX][nextY] = moveCount;
+				res = solveProblem(moveCount + 1, nextX, nextY) || res;
+				
+                // quay lui
+				visited[nextX][nextY] = Integer.MIN_VALUE; 
 			}
+		}
+		return res;
+	}
+
+	public boolean isValidMove(int x, int y) {
+		if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) {
+			return false;
+		} else {
+			return true;	
+		}
+	}
+
+	public void printSolution() {
+		System.out.println("======================================================");
+		System.out.println("Cách giải " + (++count));
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				System.out.format("%3d ", visited[i][j]);
+			}
+
 			System.out.println();
 		}
-	}
-
-	private static boolean knightsTour() {
-		// khởi tạo ma trận
-		int[][] board = new int[N][N];
-		// thiết lập ma trận với giá trị -1 là các ô vuông trên ma trận để đánh dấu là chưa sử dụng
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				board[i][j] = -1;
-			}
-		}
-		// bắt đầu từ ô (0,0)
-		board[0][0] = 0;
-	   
-		// định nghĩa sẵn các bước đi của con mã (đi với hình chữ L)
-		int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2};
-		int[] yMove = {-1, -2, -2, -1, 1, 2, 2, 1};
-	  
-		// nếu sau khi chạy hàm đệ quy, không có nghiệm thì
-		if (!tourUtility(0, 0, 1, board, xMove, yMove)) {
-			System.out.println("No solutions");
-			return false;
-		}
-		else {
-			printBoard(board);
-			return true;
-		}
-
-	}
-
-	// đệ quy
-	private static boolean tourUtility(int x, int y, int move, int[][] board, int[] xMove, int[] yMove) {
-	// nếu số bước đi là n^2 thì trả về true
-		if (move == N * N) {
-			return true;
-		}
-
-		int next_x, next_y;
-
-		for (int k = 0; k < N; k++) {
-			// di chuyển
-			next_x = x + xMove[k];
-			next_y = y + yMove[k];
-			if (isValid(next_x, next_y, board)) {
-			   // nếu ô cờ chưa từng được đi thì thêm số bước 
-				board[next_x][next_y] = move;
-				if (tourUtility(next_x, next_y, move+1, board, xMove, yMove)) {
-					return true;
-				} else {
-				   // quay lui nếu gọi hàm đệ quy lỗi và bắt đầu lại từ đầu
-					board[next_x][next_y] = -1;
-				}
-			}
-		}
-		return false;
+		System.out.println("\n");
 	}
 
 	public static void main(String[] args) {
-		knightsTour();
+		final int chess_board_size = 5;
+		KnightTour knightTour = new KnightTour(chess_board_size);
+		knightTour.solveKnightTourProblem();	
 	}
 }
